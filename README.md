f4466d2ced7757c304d1e4a14dd160fbbb542f51
Muhammad Mominul Huque <mominul2082@gmail.com>
2021-07-03 22:26:39 +0600

    Add docs and prepare for a release

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# mul
-A Bengali stemmer Rust crate
+# `mul`
+A Bengali stemmer written in Rust.
+
+`mul` currently implements a stepwise approach to removing inflections from Bengali words <sup>[1](#references)</sup>.
+
+## Example
+``` rust
+use mul::noun_stemmer;
+
+fn main() {
+    assert_eq!(noun_stemmer("বাংলায়"), "বাংলা");
+    assert_eq!(noun_stemmer("মানুষকে"), "মানুষ");
+}
+```
+
+## References
+1. M. R. Mahmud, M. Afrin, M. A. Razzaque, E. Miller and J. Iwashige, "A rule based bengali stemmer,"
+_2014 International Conference on Advances in Computing, Communications and Informatics (ICACCI)_,
+2014, pp. 2750-2756, doi: 10.1109/ICACCI.2014.6968484.
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,33 @@
+//! A Bengali stemmer
+//!
+//! `mul` currently implements a stepwise approach to removing inflections from Bengali words <sup>[1](#references)</sup>.
+//!
+//! ## Example
+//! ```
+//! use mul::noun_stemmer;
+//!
+//! fn main() {
+//!     assert_eq!(noun_stemmer("বাংলায়"), "বাংলা");
+//!     assert_eq!(noun_stemmer("মানুষকে"), "মানুষ");
+//! }
+//! ```
+//!
+//! ## References
+//! 1. M. R. Mahmud, M. Afrin, M. A. Razzaque, E. Miller and J. Iwashige, "A rule based bengali stemmer,"
+//! _2014 International Conference on Advances in Computing, Communications and Informatics (ICACCI)_,
+//! 2014, pp. 2750-2756, doi: 10.1109/ICACCI.2014.6968484.
+
+/// Removes noun inflections such as `টি`, `কে`, `তে` etc from the `input` and returns it.
+///
+/// ```
+/// use mul::noun_stemmer;
+/// 
+/// # fn main() {
+/// assert_eq!(noun_stemmer("মানুষদেরকে"), "মানুষ");
+/// assert_eq!(noun_stemmer("গাছগুলোতে"), "গাছ");
+/// assert_eq!(noun_stemmer("বাসাতে"), "বাসা");
+/// # }
+/// ```
 pub fn noun_stemmer(input: &str) -> String {
     let mut buffer = input.to_owned();
     let mut remove_y = true;
